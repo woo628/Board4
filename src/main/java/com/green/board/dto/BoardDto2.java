a9f07410a1fe1b414b2e005dddf0dda652ecf311
woo628 <duddn2500@gmail.com>
2026-05-06 17:39:31 +0900

    board 4

--- src/main/java/com/green/board/dto/BoardDto2.java
+++ src/main/java/com/green/board/dto/BoardDto2.java
@@ -1,8 +1,18 @@
 package com.green.board.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data // Getter,Setter,toString,HashCode,equals,construct
+//@Data // Getter,Setter,toString,HashCode,equals,constructor
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor // 기본생성자
+@AllArgsConstructor // 모든인자생성자
 public class BoardDto2 {
 	 private int idx;
 	 private String menu_id;
@@ -12,3 +22,4 @@ public class BoardDto2 {
 	 private String regdate;
 	 private int hit;
 }
+
